3ed037ef74405821c24bd5db1c8398e3642e1fa3
jr-villalta <jr.villalta99@gmail.com>
2023-11-14 13:49:06 -0600

    Corecciones en inventario

--- resources/views/inventario/index.blade.php
+++ resources/views/inventario/index.blade.php
@@ -81,9 +81,7 @@
             </div>
         @endif
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.5/xlsx.full.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@latest"></script>
 
