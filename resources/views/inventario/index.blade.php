b9b1707978569e00786b5446e829b1118dbe35dc
jr-villalta <jr.villalta99@gmail.com>
2023-11-14 13:52:01 -0600

    Olvidaba agregar JQuery

--- resources/views/inventario/index.blade.php
+++ resources/views/inventario/index.blade.php
@@ -81,7 +81,8 @@
             </div>
         @endif
     </div>
-    
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.5/xlsx.full.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@latest"></script>
 
