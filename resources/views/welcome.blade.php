6041fabc7b9e7a7d36a77510f86fc705d1045eec
Alexthian19 <ge19008@ues.edu.sv>
2023-11-15 23:10:27 -0600

    Cambio en el diseño de registrarse para registros tipo empresa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -178,6 +178,14 @@
 </div>
 </div>
 
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
         <!-- Modal -->
         @if (auth()->check())
             <form method="POST" action="{{ route('profile.update') }}">
@@ -209,5 +217,6 @@
             </form>
         @endif
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
     </body>
 </html>
