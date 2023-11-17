c1996ffc27a69d11d6627c5a1372a68706881a3b
jr-villalta <jr.villalta99@gmail.com>
2023-11-16 21:50:13 -0600

    Cambios en los seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,13 +16,28 @@ class UserSeeder extends Seeder
         \App\Models\User::create([
             'name' => 'admin',
             'email' => 'admin',
-            'password' => Hash::make('admin'),
+            'password' => Hash::make('vf19012@ues.edu.sv'),
             'level' => 'Admin'
         ]);
 
         \App\Models\User::create([
             'name' => 'junior',
             'email' => 'jr.villalta99@gmail.com',
+            'password' => Hash::make('user'),
+            'level' => 'User'
+        ]);
+
+        \App\Models\User::create([
+            'name' => 'Cristian',
+            'email' => 'ge190008@ues.edu.sv',
+            'password' => Hash::make('admin'),
+            'level' => 'admin'
+        ]);
+
+        // admin
+        \App\Models\User::create([
+            'name' => 'admin',
+            'email' => 'admin',
             'password' => Hash::make('admin'),
             'level' => 'Admin'
         ]);
